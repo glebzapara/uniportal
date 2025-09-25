3638bce1ea76e62db655c36afe13e4496b4acc38
Gleb Zapara <gleb.zapara2008@gmail.com>
2025-09-26 00:37:04 +0300

    s

--- src/main/java/com/glebzapara/test_project/repositories/StudentRepository.java
+++ src/main/java/com/glebzapara/test_project/repositories/StudentRepository.java
@@ -8,5 +8,4 @@ import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
-    Optional<Student> findByName(String name);
 }
--- src/main/java/com/glebzapara/test_project/services/SubjectService.java
+++ src/main/java/com/glebzapara/test_project/services/SubjectService.java
@@ -1,8 +1,14 @@
 package com.glebzapara.test_project.services;
 
+import com.glebzapara.test_project.repositories.SubjectRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class SubjectService {
+    SubjectRepository subjectRepository;
+
+    public SubjectService(SubjectRepository subjectRepository) {
+        this.subjectRepository = subjectRepository;
+    }
 }
