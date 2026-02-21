53811aa08927af31b7dc827bbd1bc3b80afa2446
Gleb Zapara <gleb.zapara2008@gmail.com>
2026-02-21 05:32:47 +0200

    update-new

--- src/main/java/com/glebzapara/test_project/config/SecurityConfig.java
+++ src/main/java/com/glebzapara/test_project/config/SecurityConfig.java
@@ -12,11 +12,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 @Configuration
 @EnableWebSecurity
